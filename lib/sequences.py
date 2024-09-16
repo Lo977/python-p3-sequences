fcaa12c115f922dae5572c374257ed05118eb512
Buddha Lo <lo_977@yahoo.com>
2024-09-16 01:24:23 -0700

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_num = [0,1]
+    while len(fib_num)<length:
+        fib_num.append(fib_num[-1]+fib_num[-2])
+    print(fib_num[:length])
